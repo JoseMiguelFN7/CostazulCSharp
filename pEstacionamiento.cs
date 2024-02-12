811b2a05ab55d935d285ad0c88637ed8a73af823
José Ferreira <147115145+JoseMiguelFN7@users.noreply.github.com>
2024-02-12 18:46:18 -0400

    primer commit

---
+++ pEstacionamiento.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Costazul
+{
+    internal class pEstacionamiento
+    {
+        private String sector;
+        private int numero;
+        private ArrayList ocupantes;
+
+        public pEstacionamiento(String sector, int numero)
+        {
+            this.sector = sector;
+            this.numero = numero;
+            this.ocupantes = new ArrayList();
+        }
+
+        public String getSector()
+        {
+            return sector;
+        }
+
+        public int getNumero()
+        {
+            return numero;
+        }
+
+        public ArrayList getOcupantes()
+        {
+            return ocupantes;
+        }
+    }
+}

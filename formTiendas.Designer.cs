81b7225f5f790292e4a6ba5147aadb44ba9d03e7
José Ferreira <147115145+JoseMiguelFN7@users.noreply.github.com>
2024-02-16 10:34:05 -0400

    agregado form tiendas

---
+++ formTiendas.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Costazul
+{
+    partial class formTiendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fondo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fondo
+            // 
+            this.fondo.Image = global::Costazul.Properties.Resources.MapaCostazulTiendas;
+            this.fondo.Location = new System.Drawing.Point(0, 0);
+            this.fondo.Name = "fondo";
+            this.fondo.Size = new System.Drawing.Size(1665, 642);
+            this.fondo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fondo.TabIndex = 0;
+            this.fondo.TabStop = false;
+            // 
+            // formTiendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1665, 643);
+            this.Controls.Add(this.fondo);
+            this.Name = "formTiendas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formTiendas";
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox fondo;
+    }
+}

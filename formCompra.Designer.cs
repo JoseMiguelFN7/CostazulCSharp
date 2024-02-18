b5bd51ab086cc9a2dbf39e7cbc5f404ed9fbec35
José Ferreira <147115145+JoseMiguelFN7@users.noreply.github.com>
2024-02-18 14:16:16 -0400

    creado el form de compras
    PARTE 1

---
+++ formCompra.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Costazul
+{
+    partial class formCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formCompra));
+            this.fondo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fondo
+            // 
+            this.fondo.Image = global::Costazul.Properties.Resources.fondo;
+            this.fondo.Location = new System.Drawing.Point(0, 0);
+            this.fondo.Name = "fondo";
+            this.fondo.Size = new System.Drawing.Size(673, 900);
+            this.fondo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fondo.TabIndex = 0;
+            this.fondo.TabStop = false;
+            // 
+            // formCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 900);
+            this.Controls.Add(this.fondo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "formCompra";
+            this.Text = "Ingrese la informacion para la compra";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formCompra_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox fondo;
+    }
+}
---
+++ formCompra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Costazul
+{
+    public partial class formCompra : Form
+    {
+        bool buttonPressed = false;
+        public formCompra()
+        {
+            InitializeComponent();
+            this.ClientSize = fondo.Size;
+        }
+
+        private void formCompra_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!buttonPressed)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
